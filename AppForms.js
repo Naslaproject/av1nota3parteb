d411ad59cf3054da879ee2b795141a0eec432e42
Naslaproject <naslaalves208@gmail.com>
2023-10-03 20:23:45 -0300

    aaaa

---
+++ AppForms.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+ 
+export default function AppForms() {
+  return (
+    <View style={styles.container}>
+      <Text>Form!</Text>
+      <StatusBar style="light" />
+    </View>
+  );
+}
+ 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#D93600',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
